949a90b85816b66303386aeedc7cb097c05e4ea2
Patrick Ethier <patrick@secureops.com>
2019-10-11 09:49:57 +0200

    Lambda@Edge functions don't support environment variables so we'll dynamically generate them at packaging time.

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,11 +1,12 @@
 'use strict';
 const util = require('util');
+const env = require('./env.js')
 const {
   URLSearchParams
 } = require('url');
 
 exports.handler = (event, context, lambda_return_cb) => {
-  const BEHAVIORPATH = process.env.BEHAVIORPATH;
+  const BEHAVIORPATH = env.CF_PREFIX;
   const cfrequest = event.Records[0].cf.request;
   console.log(`Checking ${BEHAVIORPATH} against ${cfrequest.uri}`);
   if(cfrequest.uri.indexOf(BEHAVIORPATH) === 0){
